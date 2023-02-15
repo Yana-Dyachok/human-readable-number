ecd4b25fb0beeaea500924d0de760166e29b77e6
Yana-Dyachok <97878430+Yana-Dyachok@users.noreply.github.com>
2023-02-15 11:24:14 +0200

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function toReadable (number) {
-  
+  const numToTwenty = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+  const tensNumb = ['','','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+  let decimal = number % 10
+  if (number < 20) return numToTwenty[number]
+  if (number < 100)return decimal!==0?`${tensNumb[Math.floor(number / 10)]}-${numToTwenty[decimal]}`:`${tensNumb[Math.floor(number / 10)]}`
+  if (number < 1000) return number % 100 === 0 ?`${numToTwenty[Math.floor(number / 100)]} hundred`:`${numToTwenty[Math.floor(number / 100)]} hundred ${toReadable(number % 100)}`  
 }
