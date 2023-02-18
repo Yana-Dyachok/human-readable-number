2c7756bf0e58b04e27410addae2e53527aed1f23
Yana-Dyachok <97878430+Yana-Dyachok@users.noreply.github.com>
2023-02-18 11:19:58 +0200

    fix: add `` in 8 line

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ module.exports = function toReadable (number) {
   if (number < 20) return numToTwenty[number]
   if (number < 100)return decimal!==0?`${tensNumb[Math.floor(number / 10)]}-${numToTwenty[decimal]}`:`${tensNumb[Math.floor(number / 10)]}`
   if (number < 1000) return number % 100 === 0 ?`${numToTwenty[Math.floor(number / 100)]} hundred`:`${numToTwenty[Math.floor(number / 100)]} hundred ${toReadable(number % 100)}`  
-   if (number < 1000) return number % 100 === 0 ?${numToTwenty[Math.floor(number / 100)]} hundred:${numToTwenty[Math.floor(number / 100)]} hundred ${toReadable(number % 100)};
+  if (number < 1000) return number % 100 === 0 ?`${numToTwenty[Math.floor(number / 100)]} hundred`:`${numToTwenty[Math.floor(number / 100)]} hundred ${toReadable(number % 100)}`;
 }
