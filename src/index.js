8fd3378925e0ad525af2eee770a0d61e5b94ffc9
Yana-Dyachok <97878430+Yana-Dyachok@users.noreply.github.com>
2023-02-18 11:50:23 +0200

    fix: delete '-' in 6 line

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ module.exports = function toReadable (number) {
   const tensNumb = ['','','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
   let decimal = number % 10
   if (number < 20) return numToTwenty[number]
-  if (number < 100)return decimal!==0?`${tensNumb[Math.floor(number / 10)]}-${numToTwenty[decimal]}`:`${tensNumb[Math.floor(number / 10)]}`
+  if (number < 100)return decimal!==0?`${tensNumb[Math.floor(number / 10)]} ${numToTwenty[decimal]}`:`${tensNumb[Math.floor(number / 10)]}`
   if (number < 1000) return number % 100 === 0 ?`${numToTwenty[Math.floor(number / 100)]} hundred`:`${numToTwenty[Math.floor(number / 100)]} hundred ${toReadable(number % 100)}`  
   if (number < 10000) return number % 1000 === 0 ?`${numToTwenty[Math.floor(number / 1000)]} thousand`:`${numToTwenty[Math.floor(number / 1000)]} thousand ${toReadable(number % 1000)}` 
 }
